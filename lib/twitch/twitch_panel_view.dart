de56826f34579e31f2a2420211bf7df2752cbfa5
diegokua-rgb00 <diegokua@gmail.com>
2026-08-28 18:54:51 -0300

    Update twitch_panel_view.dart

--- lib/twitch/twitch_panel_view.dart
+++ lib/twitch/twitch_panel_view.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'twitch_auth.dart';
+
+class TwitchPanelView extends StatelessWidget {
+  const TwitchPanelView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final twitchAuth = TwitchAuthService();
+
+    return Center(
+      child: ElevatedButton.icon(
+        onPressed: () {
+          twitchAuth.iniciarAutenticacion();
+        },
+        icon: const Icon(Icons.login),
+        label: const Text('Conectar con Twitch'),
+      ),
+    );
+  }
+}
 
